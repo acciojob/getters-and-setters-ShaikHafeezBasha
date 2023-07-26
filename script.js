69bb94b2ce7ce43160e1be4b50bf66487854524f
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-07-26 10:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(nam,age){
+		this.nam=nam;
+		this.age=age;
+	}
+	getter(){
+		return this.nam;
+	}
+	setter(age){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${nam} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${nam} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
